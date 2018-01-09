03f73133ab4828384ba00e64157c381b85e66006
mdkashf <mmashrafi@stuy.edu>
2018-01-09 08:24:21 -0500

    Added Woo.java

---
+++ Woo.java
@@ -0,0 +1,23 @@
+public class Woo
+{
+    //Initialization of Game
+    //GameMode: Player v. Computer or Player v. Player
+    public static int[][] board = new int[5][5];
+    
+    
+
+
+    //User inputs
+
+
+    //Victory Checker
+
+
+    //AI
+
+
+    //main
+    public static void main(String [] args){
+	System.out.println(board.toString());
+    }
+}
