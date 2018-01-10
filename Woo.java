da854a57f083bc17e2f0fa83b9e8bfff623078f7
mdkashf <mmashrafi@stuy.edu>
2018-01-10 08:39:44 -0500

    Woo.java update

--- Woo.java
+++ Woo.java
@@ -5,9 +5,39 @@ public class Woo
     public static int[][] board = new int[5][5];
 
     //Game Representation
+    public static String toString2()
+    {
+	String boardRep = " |";
+	for (int x = 0; x < 5; x++)
+	    {
+		for (int y = 0; y< 5; y++)
+		    {
+		        boardRep += board [x][y] + "|";
+		    }
+
+		boardRep += "\n |";
+	    }
+	return  boardRep.substring(0, boardRep.length() - 1);
+    }
  
     
     //User inputs
+    public static void userUpdate ( int rows , int player)
+    {
+	//takes the column from user imput and updates the board according to the number of the player
+
+	
+	int colm = 0;
+
+	while ( board [colm][rows] == 0 && rows < 4 )
+	    {
+		    
+		colm++;
+		       
+	    }
+	    
+	board [colm][rows] = player;
+    }
 
 
     //Victory Checker
@@ -17,7 +47,13 @@ public class Woo
 
 
     //main
-    public static void main(String [] args){
-	System.out.println(board.toString());
+    public static void main(String [] args)
+    {
+	System.out.println(" Clean Baord");
+	System.out.println(toString2());
+
+	System.out.println("User Input Test");
+	userUpdate( 2 , 1);
+	System.out.println(toString2());
     }
 }
