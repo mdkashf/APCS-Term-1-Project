7bedd9e433fda0b0a4b232d4e3b038c85f12e2ac
mdkashf <mmashrafi@stuy.edu>
2018-01-11 08:38:38 -0500

    Woo.java update

--- Woo.java
+++ Woo.java
@@ -1,6 +1,11 @@
+
+import java.util.Scanner;
 public class Woo
 {
     //Initialization of Game
+
+   public  static boolean game = true;
+    
     //GameMode: Player v. Computer or Player v. Player
     public static int[][] board = new int[5][5];
 
@@ -26,12 +31,12 @@ public class Woo
     {
 	//takes the column from user imput and updates the board according to the number of the player
 
-	
+	rows --;
 	int colm = 0;
 
-	while ( board [colm][rows] == 0 && rows < 4 )
+	while ( board [colm][rows] == 0 && colm < 4 )
 	    {
-		    
+		   
 		colm++;
 		       
 	    }
@@ -41,6 +46,7 @@ public class Woo
 
 
     //Victory Checker
+    
 
 
     //AI
@@ -52,8 +58,20 @@ public class Woo
 	System.out.println(" Clean Baord");
 	System.out.println(toString2());
 
-	System.out.println("User Input Test");
-	userUpdate( 2 , 1);
-	System.out.println(toString2());
+	while (game == true)
+	    {
+		System.out.println("New Game");
+		System.out.println(toString2());
+		System.out.println("Turn: Player One \n Enter a column number:");
+		Scanner sc = new Scanner( System.in);
+		int input_1 = sc.nextInt();
+		userUpdate(input_1 , 1);
+		System.out.println(toString2());
+		System.out.println("Turn: Player Two \n Enter a column number:");
+		int input_2 = sc.nextInt();
+		userUpdate(input_2 , 2);
+		System.out.println(toString2());
+
+	    }
     }
 }
