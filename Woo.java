cff580450c7abafb1222078d7a5875e5671dc0c2
Daniel Jiang <danieljiang@Daniels-MacBook-Air.local>
2018-01-15 23:57:43 -0500

    added turns

--- Woo.java
+++ Woo.java
@@ -4,18 +4,24 @@ public class Woo
 {
     //Initialization of Game
 
-   public static boolean game = true;
+    public static boolean game = true;
+    public static int turn;
     
     //GameMode: Player v. Computer or Player v. Player
-    public static int[][] board = new int[5][5];
+    private static int[][] board = new int[6][7];
 
+    //Defualt Constructors
+    private Woo(){
+	turn = 0;
+    }
+    
     //Game Representation
     public static String toString2()
     {
 	String boardRep = " |";
-	for (int x = 0; x < 5; x++)
+	for (int x = 0; x < 6; x++)
 	    {
-		for (int y = 0; y< 5; y++)
+		for (int y = 0; y < 7; y++)
 		    {
 		        boardRep += board [x][y] + "|";
 		    }
@@ -30,12 +36,17 @@ public class Woo
     public static void userUpdate ( int column , int player)
     {
 	//takes the column from user imput and updates the board according to the number of the player
-
-	for(int x = 4; x >= 0; x--){
+	
+        for(int x = 5; x >= 0; x--){
 	    if(board[x][column - 1] == 0){
 		board[x][column - 1] = player;
+		turn += 1;
 		break;
 	    }
+	}
+	if(turn == 42){
+	    System.out.println("The game is a tie. Better luck next time!");
+	    game = false;
 	}
     }
 
