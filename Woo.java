fe74fb1854cc9872cff3f19ec147eee08393ba25
Daniel Jiang <danieljiang@Daniels-MacBook-Air.local>
2018-01-16 01:47:16 -0500

    2 solutions

--- Woo.java
+++ Woo.java
@@ -4,8 +4,8 @@ public class Woo
 {
     //Initialization of Game
 
-    public static boolean game = true;
-    public static int turn;
+    private static boolean game = true;
+    private static int turn;
     
     //GameMode: Player v. Computer or Player v. Player
     private static int[][] board = new int[6][7];
@@ -30,33 +30,92 @@ public class Woo
 	    }
 	return  boardRep.substring(0, boardRep.length() - 1);
     }
- 
+
+    public static boolean horizontalWin(){
+	for(int x = 5; x > 0; x--){
+	    for(int y = 0; y < 3; y++){
+		if(board[x][y] == 0){
+		    return false;
+		}
+		else{
+		    if((board[x][y] == board[x][y + 1]) && (board[x][y + 1] == board[x][y + 2]) && (board[x][y + 2] == board[x][y + 3])){
+			return true;
+		    }
+		}
+	    }
+	}
+	return false;
+    }
+	
+    public static boolean verticalWin(){
+	for(int x = 5; x > 0; x--){
+	    for(int y = 0; y < 3; y++){
+		if(board[x][y] == 0){
+		    return false;
+		}
+		else{
+		    if((board[x][y] == board[x - 1][y]) && (board[x - 1][y] == board[x - 2][y]) && (board[x - 2][y] == board[x - 3][y])){
+			return true;
+		    }
+		}
+	    }
+	}
+	return false;
+    }
+    /*
+    public static boolean diagonalRightWin(){
+	for(int x = 0; x < 2; x++){
+	    for(int y = 0; y < 6; y++){
+		if((board[x][y] == board[x + 1][y + 1]) && (board[x + 1][y + 1] == board[x + 2][y + 2]) && (board[x + 2][y + 2] == board[x + 3][y + 3])){
+		    return true;
+		}
+	    }
+	}
+	return false;
+    }
+
+    public static boolean diagonalLeftWin(){
+	for(int x = 0; x < 2; x++){
+	    for(int y = 0; y < 6; y++){
+		if((board[x][y] == board[x][y + 1]) && (board[x][y + 1] == board[x][y + 2]) && (board[x][y + 2] == board[x][y + 3])){
+		    return true;
+		}
+	    }
+	}
+	return false;
+    }
+    */
+    public static boolean winConditionMet(){
+	if(horizontalWin() || verticalWin()){
+	    return true;
+		}
+	return false;
+    }
     
     //User inputs
-    public static void userUpdate ( int column , int player)
-    {
+    public static void userUpdate(int column, int player){
 	//takes the column from user imput and updates the board according to the number of the player
-	
+
+	if (column > 7 || column < 0){
+	    column = 0;
+	}
         for(int x = 5; x >= 0; x--){
 	    if(board[x][column - 1] == 0){
 		board[x][column - 1] = player;
 		turn += 1;
+		if(winConditionMet() && (board[x][column - 1] == player) && (turn > 3)){
+		    System.out.println("Player " + player + " wins!");
+		    System.exit(0);
+		}
 		break;
 	    }
 	}
+	//ends the game in a draw if the board is filled and there is no victor
 	if(turn == 42){
 	    System.out.println("The game is a tie. Better luck next time!");
-	    game = false;
+	    System.exit(0);
 	}
     }
-
-
-    //Victory Checker
-    
-
-
-    //AI
-
 
     //main
     public static void main(String [] args)
@@ -81,3 +140,4 @@ public class Woo
 	    }
     }
 }
+
