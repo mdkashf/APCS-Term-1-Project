6ff41beb8cdb3d87f1109421c16f13731a3c2751
Daniel Jiang <danieljiang@Daniels-MacBook-Air.local>
2018-01-17 18:45:01 -0500

    Int2String

--- PvP.java
+++ PvP.java
@@ -7,17 +7,21 @@ public class PvP extends Woo
 	    {
 		System.out.println("New Game");
 		System.out.println(toString2());
-		System.out.println("Turn: Player One \n Enter a column number:");
+		System.out.println("Turn: Player One \nEnter a column number:");
 		Scanner sc = new Scanner( System.in);
 		int input_1 = sc.nextInt();
-		while(input_1< 0 || input_1 > 7){
-		    System.out.print("Invalid Number. Try again fool. \n");
+		while(input_1 < 0 || input_1 > 7){
+		    System.out.print("Invalid input. Try again fool. \n");
 		    input_1 = sc.nextInt();
 		}
 		userUpdate(input_1 , 1);
 		System.out.println(toString2());
-		System.out.println("Turn: Player Two \n Enter a column number:");
+		System.out.println("Turn: Player Two \nEnter a column number:");
 		int input_2 = sc.nextInt();
+		while(input_2 < 0 || input_2 > 7){
+		    System.out.print("Invalid input. Try again fool. \n");
+		    input_2 = sc.nextInt();
+		}
 		userUpdate(input_2 , 2);
 		System.out.println(toString2());
 
--- Woo.java
+++ Woo.java
@@ -8,7 +8,7 @@ public class Woo
     public static int turn;
     
     //GameMode: Player v. Computer or Player v. Player
-    public static int[][] board = new int[6][7];
+    public static String[][] board = new String[6][7];
 
     //Defualt Constructors
     public Woo(){
@@ -19,26 +19,41 @@ public class Woo
     public static String toString2()
     {
 	String boardRep = " |";
+	for(int a = 0; a < 6; a++){
+	    for(int b = 0; b < 7; b++){
+		if(board[a][b] == null){
+		    board[a][b] = " ";
+		}
+	    }
+	}
+	    
 	for (int x = 0; x < 6; x++)
 	    {
 		for (int y = 0; y < 7; y++)
 		    {
-		        boardRep += board [x][y] + "|";
+		        boardRep += board[x][y] + "|";
 		    }
 
 		boardRep += "\n |";
 	    }
 	boardRep = boardRep.substring(0, boardRep.length() - 1);
 	boardRep += " 1 2 3 4 5 6 7 ";
-	return  boardRep.substring(0, boardRep.length() - 1);
+	return boardRep.substring(0, boardRep.length() - 1);
     }
-        
+
+    public static String cashCoders(int player){
+	if (player == 1){
+	    return "$";
+	}
+	return "C";
+    }
+    
         //checks for horizontal victory
     public static boolean horizontalWin(int player){
 	for(int x = 5; x > 0; x--){
 	    for(int y = 0; y < 4; y++){
-		if(board[x][y] == player){
-		    if((board[x][y] == board[x][y + 1]) && (board[x][y + 1] == board[x][y + 2]) && (board[x][y + 2] == board[x][y + 3])){
+		if(board[x][y].equals(cashCoders(player))){
+		    if((board[x][y].equals(board[x][y + 1])) && (board[x][y + 1].equals(board[x][y + 2])) && (board[x][y + 2].equals(board[x][y + 3]))){
 			return true;
 		    }
 		}
@@ -51,7 +66,7 @@ public class Woo
     public static boolean verticalWin(int player){
 	for(int x = 5; x > 3; x--){
 	    for(int y = 0; y < 7; y++){
-		if(board[x][y] == player){
+		if(board[x][y].equals(cashCoders(player))){
 		    if((board[x][y] == board[x - 1][y]) && (board[x - 1][y] == board[x - 2][y]) && (board[x - 2][y] == board[x - 3][y])){
 			return true;
 		    }
@@ -65,7 +80,7 @@ public class Woo
     public static boolean diagonalGrowthWin(int player){
 	for(int x = 5; x > 3; x--){
 	    for(int y = 0; y < 4; y++){
-		if(!(board[x][y] == 0)){
+		if(board[x][y].equals(cashCoders(player))){
 		    if((board[x][y] == board[x - 1][y + 1]) && (board[x - 1][y + 1] == board[x - 2][y + 2]) && (board[x - 2][y + 2] == board[x - 3][y + 3])){
 			return true;
 		    }
@@ -79,7 +94,7 @@ public class Woo
     public static boolean diagonalDecayWin(int player){
 	for(int x = 5; x > 3; x--){
 	    for(int y = 6; y > 2; y--){
-		if(!(board[x][y] == 0)){
+		if(board[x][y].equals(cashCoders(player))){
 		    if((board[x][y] == board[x - 1][y - 1]) && (board[x - 1][y - 1] == board[x - 2][y - 2]) && (board[x - 2][y - 2] == board[x - 3][y - 3])){
 		    return true;
 		    }
@@ -105,10 +120,15 @@ public class Woo
 	//takes the column from user imput and updates the board according to the number of the player
 	
         for(int x = 5; x >= 0; x--){
-	    if(board[x][column - 1] == 0){
-		board[x][column - 1] = player;
+	    if(board[x][column - 1] == " "){
+		if (player == 1){
+		    board[x][column - 1] = "$";
+		}
+		if (player == 2){
+		    board[x][column - 1] = "C";
+		}
 		turn += 1;
-		if(winConditionMet(player) && (board[x][column - 1] == player) && (turn > 3)){
+		if(winConditionMet(player)){
 		    System.out.println(toString2());
 		    System.out.println("Player " + player + " wins!");
 		    System.exit(0);
